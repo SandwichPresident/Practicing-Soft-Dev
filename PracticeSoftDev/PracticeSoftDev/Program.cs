ec573bf36caee730a2ebcdf55da5251dae4bb137
mksigrest <mksigrest@gmail.com>
2025-11-04 08:19:30 -0500

    added searchHistory stack, and search function
    added the universal stack, search history, along with the full completed search code for the stack function with completed text

--- PracticeSoftDev/PracticeSoftDev/Program.cs
+++ PracticeSoftDev/PracticeSoftDev/Program.cs
@@ -2,9 +2,30 @@
 {
     internal class Program
     {
+        Stack<string> searchHistory = new Stack<string>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            
         }
+
+        public void search()
+        {
+            string query;
+            Console.WriteLine("What do you want to search: ");
+            query = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("You searched: " + query + "...");
+            Console.WriteLine();
+            Console.WriteLine("No results found with search: " + query);
+            Console.WriteLine();
+            Console.WriteLine(query + " is added to search history.");
+
+            searchHistory.Push(query);
+            return;
+        }
+
+
     }
 }
