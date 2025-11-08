0a833b5122bb6f8f02738d928c564ed4402944ba
BIK333 <brandk0313@gmail.com>
2025-11-08 18:06:35 -0500

    Added comments and cleaned up cod a lil bit
    Code is cleaned up and comments are now there

--- PracticeSoftDev/PracticeSoftDev/Program.cs
+++ PracticeSoftDev/PracticeSoftDev/Program.cs
@@ -1,26 +1,40 @@
-﻿namespace PracticeSoftDev
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeSoftDev
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Initialize an empty stack to store search history entries
             Stack<string> searchHistory = new Stack<string>();
+            
+            //Boolean to control the loop and int to track iterations
             bool iterate = false;
             int ret = 0;
 
             Console.WriteLine("Welcome to the Browser (with advanced search history)!");
             Console.WriteLine();
-            while (iterate == false)
+
+            //Main loop for user interaction
+            while (!iterate)
             {
                 string nyVal;
+
+                //First interaction, call logic once
                 if (ret == 0)
                 {
                     logic(searchHistory);
                 }
+
+                //Ask if the user wants to perform another action
                 Console.WriteLine("Do you want to do more?");
                 Console.Write("Input either y or n: ");
                 nyVal = Console.ReadLine();
                 Console.WriteLine();
+
                 if (nyVal == "y")
                 {
                     logic(searchHistory);
@@ -28,89 +42,88 @@
                 }
                 else if (nyVal == "n")
                 {
-                    return;
+                    return; //Ends program
                 }
             }
         }
 
+        //Handles main menu selection: search, view, or delete
         static public void logic(Stack<string> searchHist)
         {
-            string stringVal;
             Console.WriteLine("What would you like to do: Search, View, or Delete?");
             Console.Write("Input either s, v, or d: ");
-            stringVal = Console.ReadLine();
-            if (stringVal == "s")
+            string userInput = Console.ReadLine();
+
+            if (userInput == "s")
             {
                 search(searchHist);
-                return;
             }
-            else if (stringVal == "v")
+            else if (userInput == "v")
             {
                 view(searchHist);
-                return;
             }
-            else if (stringVal == "d")
+            else if (userInput == "d")
             {
                 delete(searchHist);
-                return;
             }
             else
             {
-                Console.WriteLine("Not an appropriate value");
-                return;
+                Console.WriteLine("Invalid input. Please try again.");
             }
         }
 
+        //Simulates a user search and pushes the query onto the stack
         static public void search(Stack<string> hist)
         {
-            string query;
             Console.Write("What do you want to search: ");
-            query = Console.ReadLine();
-            Console.WriteLine();
-            Console.WriteLine("You searched: " + query + "...");
-            Console.WriteLine();
-            Console.WriteLine("No results found with search: " + query);
-            Console.WriteLine();
-            Console.WriteLine(query + " is added to your search history.");
+            string query = Console.ReadLine();
             Console.WriteLine();
 
-            hist.Push(query);
-            return;
+            Console.WriteLine("You searched: " + query + "...");
+            Console.WriteLine("No results found with search: " + query);
+            Console.WriteLine(query + " has been added to your search history.");
+            Console.WriteLine();
+
+            hist.Push(query); //Adds search to history (top of the stack)
         }
 
+        //Views recent search history using Peek (non-destructive) or Pop (destructive)
         static public void view(Stack<string> hist)
         {
-            int range;
             Console.Write($"How many entries of your search history do you wish to see? LIMIT: {hist.Count} - ");
-            //casts read line into an int
-            range = int.Parse(Console.ReadLine());
+            int range;
+
+            //Parse user input into integer safely
+            bool valid = int.TryParse(Console.ReadLine(), out range);
             Console.WriteLine();
-            if (range < 0 || range > hist.Count())
+
+            if (!valid || range < 0 || range > hist.Count)
             {
-                Console.WriteLine("Out of bounds, please try again: ");
+                Console.WriteLine("Invalid number. Please try again.");
                 view(hist);
                 return;
             }
-            //tries to peek first to limit complexity/demand
-            else if (range == 1)
+
+            //Single item view uses Peek to avoid unnecessary copying
+            if (range == 1)
             {
                 Console.WriteLine(hist.Peek());
                 Console.WriteLine();
-                return;
             }
             else
             {
-                //creates a copy of hist to be used in a desctructive reading
-                Stack<String> histCopy = new Stack<String>(hist.Reverse());
-                for (int i = 0; i < range; i++) { 
+                //Create reversed copy of stack to view entries from oldest to newest
+                Stack<string> histCopy = new Stack<string>(hist.Reverse());
+
+                for (int i = 0; i < range; i++)
+                {
                     Console.WriteLine(histCopy.Pop());
                     Console.WriteLine();
                 }
-                return;
             }
-
         }
 
+        //Deletes most recent search from history using Pop
         static public void delete(Stack<string> hist)
         {
             if (hist.Count > 0)
@@ -119,14 +132,12 @@
                 Console.WriteLine();
                 Console.WriteLine("Most recent search deleted.");
                 Console.WriteLine();
-                return;
             }
             else
             {
                 Console.WriteLine();
                 Console.WriteLine("No searches to delete.");
                 Console.WriteLine();
-                return;
             }
         }
     }
